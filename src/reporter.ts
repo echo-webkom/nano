c1a442275e472047bc535ab4e0eca50d6fa00ea7
Ole Magnus Fon Johnsen <me@omfj.no>
2024-03-18 12:33:16 +0100

    add comments

--- src/reporter.ts
+++ src/reporter.ts
@@ -5,6 +5,16 @@ export const TTL = 60 * 60 * 1; // 1 hour
 
 const REPORTS_UNTIL_STRIKE = 2;
 
+/**
+ * Adds a report to the KV store. If the number of reports exceeds the threshold,
+ * the "kaffemaskin" will be given a strike.
+ *
+ * Each report has a TTL of 1 hour.
+ *
+ * @param kv the KV namespace
+ * @param reporter the id of the reporter
+ * @returns the current number of strikes
+ */
 export const add = async (
   kv: KVNamespace,
   reporter: string
@@ -21,10 +31,21 @@ export const add = async (
   return Striker.get(kv);
 };
 
+/**
+ * Gets the current number of reports.
+ *
+ * @param kv the KV namespace
+ * @returns the current number of report
+ */
 export const get = async (kv: KVNamespace): Promise<number> => {
   return kv.list({ prefix: KEY }).then((keys) => keys.keys.length);
 };
 
+/**
+ * Resets the number of reports to 0.
+ *
+ * @param kv the KV namespace
+ */
 export const reset = async (kv: KVNamespace): Promise<void> => {
   const keys = await kv.list({ prefix: KEY });
   await Promise.all(keys.keys.map((key) => kv.delete(key.name)));
