ff6abd3077a27e2bcd104fa60c153f4577895c26
Ole Magnus Fon Johnsen <me@omfj.no>
2024-05-22 22:02:11 +0200

    Upload files

--- echogram/src/index.ts
+++ echogram/src/index.ts
@@ -8,6 +8,47 @@ export type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>();
 
+app.get("/:id", async (c) => {
+  const { id } = c.req.param();
+
+  const object = await c.env.R2.get(id);
+
+  if (!object) {
+    c.status(404);
+    return c.json({ message: "Not found" });
+  }
+
+  const headers = new Headers(Object.entries(object.httpMetadata ?? {}));
+  headers.set("etag", object.etag);
+
+  return new Response(object.body, {
+    headers,
+  });
+});
+
+const SUPPORTED_FILE_TYPES = ["image/png", "image/jpeg", "image/gif"];
+
+app.post("/:id", auth, async (c) => {
+  const { id } = c.req.param();
+
+  const formData = await c.req.formData();
+
+  const file = formData.get("file") as File;
+
+  if (!file || !file.size || !SUPPORTED_FILE_TYPES.includes(file.type)) {
+    c.status(400);
+    return c.json({ message: "No file uploaded" });
+  }
+
+  const body = await file.arrayBuffer();
+
+  await c.env.R2.put(id, body);
+
+  return c.json({
+    message: "File uploaded",
+  });
+});
+
 app.get("/healthcheck", async (c) => {
   return c.json({ status: "ok" });
 });
