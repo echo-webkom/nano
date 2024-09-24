c1e757c87b9a7a9a93b5c8b033b2eebe676d4a05
Ole Magnus Fon Johnsen <me@omfj.no>
2024-09-24 21:03:19 +0200

    Remove tech debt

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -2,7 +2,5 @@ import { defineWorkspace } from "vitest/config";
 
 export default defineWorkspace([
   "./apps/crons/vitest.config.ts",
-  "./apps/kaffe/vitest.config.ts",
   "./apps/echogram/vitest.config.ts",
-  "./apps/boomtown/vitest.config.ts",
 ]);
